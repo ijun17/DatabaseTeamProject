0415c92fe235972915a1e52bc9d4470c09969d2b
ijun17 <ijun17@naver.com>
2023-06-02 00:20:22 +0900

    s

--- src/js/renderActionBar.js
+++ src/js/renderActionBar.js
@@ -3,27 +3,31 @@ const testModeCheckbox = document.getElementById('testModeCheckbox');
 
 //[이름,renderer,요청키워드]
 const actionBarList = [
-    ["스타포스",renderStarforce,"starforce"],
-    ["스타포스(파괴방지)",renderStarforcePreventBreak,"starforcePreventBreak"],
-    ["주문서 강화",renderScrollforce,"scrollforce"],
-    ["주문서 강화(파괴방지)",renderScrollforcePreventBreak,"scrollforcePreventBreak"],
-    ["장비 아이템 획득",renderJangItemGet,"um"],
-    ["소비 아이템 획득",renderSoItemGet,"jun"],
-    ["소비 아이템 사용",renderSoItemUse,"sick"],
-    ["월드아이템 구매",renderItemChart,"worldItemBuy"]
+    ["스타포스",renderStarforce,"/starforce"],
+    ["스타포스(파괴방지)",renderStarforcePreventBreak,"/starforceD"],
+    ["주문서 강화",renderScrollforce,"/scrollforce"],
+    ["주문서 강화(파괴방지)",renderScrollforcePreventBreak,"/scrollforceD"],
+    ["장비 아이템 획득",renderJangItemGet,"/getItem"],
+    ["소비 아이템 획득",renderSoItemGet,"/getItemConsume"],
+    ["소비 아이템 사용",renderSoItemUse,"/useItem"],
+    ["월드아이템 구매",renderItemChart,"/purchaseItem"]
 ]
 
 function getUserID(){
-    let value = document.querySelector("input").value;
-    return value;
+    let value = document.getElementById("userid").value;
+    if(value.length=="")return "";
+    else return "/"+value;
 }
 
-async function fetchData(key) {
-    let url="http://localhost:3001"//'https://minework.shop/now'
-    return fetch(url+"/"+key+"/"+getUserID())
+async function fetchData(key, callback) {
+    let root="http://localhost:3001"
+    let url=root+key+getUserID()//'https://minework.shop/now'
+    console.log(url)
+    fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            callback(data);
         })
         .catch(error => console.error('Error:', error));
 }
@@ -36,12 +40,8 @@ function renderActionBar(){
         btn.innerText=actionBarList[i][0];
         btn.className="action-button"
         btn.onclick=async function(){
-            if(testModeCheckbox.checked){
-                actionBarList[i][1](TEST_DATASET[i]);
-            }else{
-                let data = await fetchData(actionBarList[i][2]);
-                actionBarList[i][1](data);
-            }
+            if(testModeCheckbox.checked) actionBarList[i][1](TEST_DATASET[i]);
+            else fetchData(actionBarList[i][2], actionBarList[i][1]);
         }
         actionBarEle.appendChild(btn);
     }
